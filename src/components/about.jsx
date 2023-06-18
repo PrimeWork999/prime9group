846a3e8da3060e25c1472cb4c7605236cf72d04a
Kuldeep <kuldeepchotrani>
2023-06-18 22:53:04 +0530

    video fix

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -30,7 +30,7 @@ export const About = (props) => {
         <div className='row'>
           <div className='col-xs-12 col-md-6'>
             {' '}
-            <img src='img/PRIME999.png' className='img-responsive' alt='' />{' '}
+            <img src='../img/PRIME999.png' className='img-responsive' alt='' />{' '}
           </div>
           <div className='col-xs-12 col-md-6'>
             <div className='about-text'>
